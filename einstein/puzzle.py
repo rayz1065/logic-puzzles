bb939651c90f60099208d8fc9c061ad024e24e30
rayz1065 <rayz1065@gmail.com>
2024-03-05 17:27:07 +0100

    Einstein: improved solver speed using new branching solver, simplified code

--- einstein/puzzle.py
+++ einstein/puzzle.py
@@ -24,6 +24,7 @@ class EinsteinPuzzle(Puzzle):
     def __init__(self, hints, items_by_type, state=None):
         self.hints = hints
         self.items_by_type = items_by_type
+        self.state = state
         self.items = []
         for item_type, items in self.items_by_type.items():
             self.items.extend(items)
@@ -34,24 +35,26 @@ class EinsteinPuzzle(Puzzle):
                 self.hints_by_item[item].append(hint)
 
         if state is None:
-            houses = [[] for _ in self.items_by_type["PERSON"]]
-            item_locations = {item: None for item in self.items}
-            conflict_values = [
-                {item: 0 for item in self.items} for _ in self.items_by_type["PERSON"]
-            ]
-            self.state = EinsteinState(houses, item_locations, conflict_values)
+            self.initialize_state()
 
-            for i, item in enumerate(self.items_by_type["PERSON"]):
-                self.set_house(item, i)
-        else:
-            self.state = state
+    def initialize_state(self):
+        self.state = EinsteinState(
+            houses=[[] for _ in self.items_by_type["PERSON"]],
+            item_location={item: None for item in self.items},
+            conflict_values=[
+                {item: 0 for item in self.items} for _ in self.items_by_type["PERSON"]
+            ],
+        )
+
+        for i, item in enumerate(self.items_by_type["PERSON"]):
+            self.set_value(item, i)
 
     def __str__(self):
         res = ["# Hints"]
         res.extend((f"- {hint}" for hint in self.hints))
         res.append("# State")
         for person in self.state.houses:
-            res.append("- " + ", ".join(map(str, person)))
+            res.append("- " + ", ".join(map(str, sorted(person))))
 
         return "\n".join(res)
 
@@ -84,54 +87,39 @@ class EinsteinPuzzle(Puzzle):
         return cls(hints, items_by_type)
 
     def _update_conflict_values(self, item, house, delta):
-        dirty = set()
-
         for hint in self.hints_by_item[item]:
             other_item = hint.item_1 if hint.item_2 == item else hint.item_2
 
             if hint.negated:
                 # other item must not be in the same house
                 self.state.conflict_values[house][other_item] += delta
-                if (
-                    self.state.conflict_values[house][other_item] == delta
-                    and self.state.item_location[other_item] is None
-                ):
-                    dirty.add(other_item)
                 continue
 
             # other item must not be in a different house
             for other_house in range(len(self.state.houses)):
                 if other_house != house:
                     self.state.conflict_values[other_house][other_item] += delta
-                    if (
-                        self.state.conflict_values[other_house][other_item] == delta
-                        and self.state.item_location[other_item] is None
-                    ):
-                        dirty.add(other_item)
 
         # no two items of the same type in the same home
         for other_item in self.items_by_type[item.item_type]:
             if other_item != item:
                 self.state.conflict_values[house][other_item] += delta
-                if (
-                    self.state.conflict_values[house][other_item] == delta
-                    and self.state.item_location[other_item] is None
-                ):
-                    dirty.add(other_item)
 
-        return dirty
+    def get_valid_values(self, item):
+        return [i for i, house in enumerate(self.state.houses) if self.can_set(item, i)]
 
-    def set_house(self, item, house):
+    def can_set(self, item, house):
+        return self.state.conflict_values[house][item] == 0
+
+    def set_value(self, item, house):
         assert self.state.item_location[item] is None
         self.state.houses[house].append(item)
         self.state.item_location[item] = house
+        self._update_conflict_values(item, house, 1)
 
-        return self._update_conflict_values(item, house, 1)
-
-    def unset_house(self, item):
+    def unset_value(self, item):
         house = self.state.item_location[item]
         assert house is not None
         self.state.houses[house].remove(item)
         self.state.item_location[item] = None
-
         self._update_conflict_values(item, house, -1)
--- einstein/solver.py
+++ einstein/solver.py
@@ -1,57 +1,30 @@
 import sys
-from logic_puzzles.solver import Solver
+from logic_puzzles.solver import SimpleBranchingSolver
 from .puzzle import EinsteinPuzzle, EinsteinState
 
 
-class EinsteinSolver(Solver):
+class EinsteinSolver(SimpleBranchingSolver):
     puzzle: EinsteinPuzzle
 
-    def _solve_dirty(self, item_idx, dirty):
-        self.check_timeout()
+    def _compute_dirty(self, item):
+        dirty = set()
+        for hint in self.puzzle.hints_by_item[item]:
+            other_item = hint.item_1 if hint.item_2 == item else hint.item_2
 
-        if len(dirty) == 0:
-            return self._solve(item_idx)
+            if not self.is_location_set(other_item):
+                dirty.add(other_item)
 
-        item = dirty.pop()
-        if self.state.item_location.get(item) is not None:
-            return self._solve_dirty(item_idx, dirty)
+        for other_item in self.puzzle.items_by_type[item.item_type]:
+            if not self.is_location_set(other_item):
+                dirty.add(other_item)
 
-        valid_houses = [
-            i
-            for i, house in enumerate(self.state.houses)
-            if self.state.conflict_values[i][item] == 0
-        ]
-        if len(valid_houses) == 0:
-            return 0
+        return dirty
 
-        if len(valid_houses) != 1:
-            return self._solve_dirty(item_idx, dirty)
+    def get_branching_score(self, location):
+        return -sum(self.puzzle.get_valid_values(location))
 
-        house = valid_houses[0]
-        dirty.update(self.puzzle.set_house(item, house))
-        res = self._solve_dirty(item_idx, dirty)
-        self.puzzle.unset_house(item)
+    def is_location_set(self, location):
+        return self.state.item_location.get(location) is not None
 
-        return res
-
-    def _solve(self, item_idx=0):
-        self.check_timeout()
-
-        if item_idx == len(self.puzzle.items):
-            self.store_solution()
-            return 1
-
-        item = self.puzzle.items[item_idx]
-        if self.state.item_location.get(item) is not None:
-            return self._solve(item_idx + 1)
-
-        res = 0
-        for i, house in enumerate(self.state.houses):
-            if self.state.conflict_values[i][item] > 0:
-                continue
-
-            dirty = self.puzzle.set_house(item, i)
-            res += self._solve_dirty(item_idx + 1, dirty)
-            self.puzzle.unset_house(item)
-
-        return res
+    def iter_locations(self):
+        yield from self.puzzle.items
